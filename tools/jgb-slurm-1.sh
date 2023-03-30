374836e49fdde0b25c3bdf447d06de2aa40398b2
JosephGeoBenjamin <josephgeobenjamin@gmail.com>
2023-03-31 01:46:04 +0400

    [slurm] Mind bogging issue with nested bash-scripts Simple single code running set up in tools

--- tools/jgb-slurm-1.sh
+++ tools/jgb-slurm-1.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=exp-DfDt-1             # Job name
-#SBATCH --output=/nfs/users/ext_cvgroup-9/jgeob/OUTDIR/slurm-logs/exp-DfDt1_%A_%N.txt # Standard output and error log
+#SBATCH --job-name=exp-DfDt-2             # Job name
+#SBATCH --output=/nfs/users/ext_cvgroup-9/jgeob/OUTDIR/slurm-logs/Ex2-DfDt1_%A_%N.txt # Standard output and error log
 #SBATCH --nodes=1                   # Run all processes on a single node
-#SBATCH --ntasks=1                  # Run on a single CPU
+#SBATCH --ntasks=2                  # Run on a single CPU
 #SBATCH --mem=80G                   # Total RAM to be used
 #SBATCH --cpus-per-task=32          # Number of CPU cores
-#SBATCH --gres=gpu:1                # Number of GPUs (per node)
-#SBATCH --time=12:00:00             # Specify the time needed for your experiment
+#SBATCH --gres=gpu:2                # Number of GPUs (per node)
+#SBATCH --time=24:00:00             # Specify the time needed for your experiment
 #SBATCH --reservation=cv703         # for partition allocation
 #SBATCH --partition=cv703           # for partition allocation
 
 GPUS=2
-PORT="29501"
+PORT="29500"
+EXP_DIR=/nfs/users/ext_cvgroup-9/jgeob/OUTDIR/E01-r50_deformable_detr-baseline-2
+RESUMEPTH=/nfs/users/ext_cvgroup-9/jgeob/OUTDIR/E01-r50_deformable_detr-baseline-1/checkpoint.pth
 ROOTPATH=~/jgeob/rotb/
 
 hostname
@@ -19,12 +21,21 @@ date
 # conda activate cv703
 cd ${ROOTPATH}
 
-## copy from run_dist_launch.sh
+## -----------------------------------------------------------------------------
 set -x
 
 GPUS_PER_NODE=${GPUS}
 
-RUN_COMMAND="./configs/r50_deformable_detr.sh"
+RUN_COMMAND="python -u main.py \
+    --batch_size 4 \
+    --epochs 55 \
+    --output_dir ${EXP_DIR} \
+    --resume ${RESUMEPTH} \
+    --isaid_path /nfs/projects/cv703/jazz-cvgroup-9/ \
+    --detection_pretrained pretrained/r50_deformable_detr-checkpoint.pth
+    "
+
+# RUN_COMMAND="./configs/r50_deformable_detr.sh"
 
 if [ $GPUS -lt 8 ]; then
     GPUS_PER_NODE=${GPUS_PER_NODE:-$GPUS}
@@ -45,3 +56,5 @@ python ./tools/launch.py \
     --master_port ${MASTER_PORT} \
     --nproc_per_node ${GPUS_PER_NODE} \
     ${RUN_COMMAND}
+
+echo "DONE TRAINING !!!"
